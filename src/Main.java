0ebf1e46f9e093c8c01b836e453ed4a3570dccd2
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-01-16 22:42:25 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 100;
+        int depositAmount = 1510;
+        int bonusCalculation = depositAmount / 100;
+
+        if (bonusCalculation < 1000) {
+            System.out.println("Итоговый счет:" + (cost + depositAmount + bonusCalculation));
+            System.out.println("Количество бонусов:" + bonusCalculation);
+        } else {
+            System.out.println("Итоговый счет:" + (cost + depositAmount));
+        }
+    }
+}
